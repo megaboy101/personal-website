1bf02f22d75ab0220ac853e320bd3811186e8308
Jacob Bleser <bleserjacob@gmail.com>
2023-12-31 00:07:12 -0500

    Added Turbo

--- web/templates/document.tsx
+++ web/templates/document.tsx
@@ -8,9 +8,7 @@ export default ({ children }: { children?: Child } ) => (
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" />
+      <script type="module" defer src="https://cdn.skypack.dev/@hotwired/turbo@7.3?min"></script>
 
       <link rel="stylesheet" href="/style/tokens.css" />
       <link rel="stylesheet" href="/style/preflight.css" />
