9fad7ddf64e63ae2201c353f5fda6fe6653589f0
Jacob Bleser <bleserjacob@gmail.com>
2024-01-18 13:39:07 -0500

    Updated landing page copy

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -5,21 +5,39 @@ const page = new Hono()
 page.get("", c => {
   return c.render(
     <div layout="pt-8">
-      <h1 layout="pb-5">
+      <h1 variant="small" layout="pb-5">
         👋 Hi, I'm Jacob
       </h1>
 
-      <p variant="large">
-        I’m an indie web and game developer currently living the dream
-        in ✨ New York City ✨. In college I co-founded a small web dev
-        shop called Studio Reach to help bootstrap local tech startups,
-        and more recently led the engineering efforts at a VC-backed
-        social-dating startup called The Marriage Pact, along with the
-        Checkmate app.
+      <p>
+        I build apps and games for the web.
         <br/><br/>
-        Outside of work I’m also active in the open source community,
-        primarily in Elixir and the Bevy game engine for Rust.
+        Right now I work as a product engineer at <a href="https://discord.com/" style="color:#5865F2;font-weight:500;border-bottom:1px solid #5865F2">Discord</a>, and before that I was a founding engineer at a social dating startup called <a href="https://marriagepact.com/" style="color:#ff003d;font-weight:500;border-bottom:1px solid #ff003d">The Marriage Pact</a>.
+        <br/><br/>
+        I currently live in Brooklyn, New York
       </p>
+
+      <div layout="row gap-6 py-8 middle">
+        <div style="background:var(--neutral-9);border-radius:100px;width:3px;height:3px"></div>
+        <div style="background:var(--neutral-9);border-radius:100px;width:3px;height:3px"></div>
+        <div style="background:var(--neutral-9);border-radius:100px;width:3px;height:3px"></div>
+      </div>
+
+      <p>
+        🌱 This is my <a href="https://maggieappleton.com/garden-history" style="color:rgb(4, 164, 186);font-weight:500;border-bottom:1px solid rgb(4, 164, 186)">digital garden</a>, where I collect my thoughts, experiences, and doodles.
+        <br/><br/>
+        ❗️ Not everything here is polished or complete, but that's the beauty of it. Life, much like software, is perpetually under construction.
+      </p>
+
+      <h2 variant="small" layout="pt-8 pb-2">Connect with me</h2>
+
+      <div layout="row gap-3">
+        <a href="https://discordapp.com/users/318894685714120706" style="color:#5865F2;font-weight:500;border-bottom:1px solid #5865F2">Discord</a>
+        <a href="https://twitter.com/jacobbleser" style="color:#14171a;font-weight:500;border-bottom:1px solid #14171a">Twitter</a>
+        <a href="https://github.com/megaboy101" style="color:#181717;font-weight:500;border-bottom:1px solid #181717">Github</a>
+        <a href="https://www.linkedin.com/in/jacobbleser/" style="color:#0A66C2;font-weight:500;border-bottom:1px solid #0A66C2">LinkedIn</a>
+        <a href="https://docs.google.com/document/d/1WGUI6Ib-jkLDm7kzfFZwINk2x1Wi3TQjopYROJYNbAk/edit?usp=sharing" style="color:#4285F4;font-weight:500;border-bottom:1px solid #4285F4">Resume</a>
+      </div>
     </div>
   )
 })
