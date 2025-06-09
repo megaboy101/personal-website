aca35ec39658392d797468d650a4ebe30a70514f
Jacob Bleser <bleserjacob@gmail.com>
2025-06-08 20:28:00 -0400

    More styling cleanup

--- layout/header.tsx
+++ layout/header.tsx
@@ -4,18 +4,21 @@ export default () => {
   return (
     <header>
       <div class="profile">
-        <a href="/"><img src="/img/pfp.jpg" alt="Profile picture" /></a>
+        <a href="/"><img src="/img/card-gradient.jpg" alt="Profile picture" /></a>
         <h1><a href="/">Jacob Bleser</a></h1>
-        <address>
+        {/* <address>
           <a href="mailto:bleserjacob@gmail.com"><Email /></a>
           <a href="https://bsky.app/profile/jacobb.nyc"><Bluesky /></a>
           <a href="https://github.com/megaboy101"><Github /></a>
           <a href="https://linkedin.com/in/jacobbleser"><LinkedIn /></a>
-        </address>
+        </address> */}
       </div>
 
       <div class="settings" role="group">
-        <button id="theme-toggle" type="button" aria-pressed="false">
+        <a id="bluesky" type="button" href="https://bsky.app/profile/jacobb.nyc">
+          <Bluesky /> 37
+        </a>
+        <button id="theme-toggle" type="button">
           <Sun />
         </button>
       </div>
