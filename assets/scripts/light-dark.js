87f77990b9292c134f54b7b32c78fd8acad2ec64
Jacob Bleser <bleserjacob@gmail.com>
2025-08-02 15:46:42 -0400

    Fixed light-dark button not working when multiple buttons

--- assets/scripts/light-dark.js
+++ assets/scripts/light-dark.js
@@ -1,35 +1,36 @@
-let html
-let btn
+let html;
+let btns;
 
-if (document.readyState !== 'loading') {
-  init()
+if (document.readyState !== "loading") {
+  init();
 } else {
-  document.addEventListener('DOMContentLoaded', init)
+  document.addEventListener("DOMContentLoaded", init);
 }
 
 function init() {
-  html = document.querySelector('html')
-  btn = document.querySelector('#theme-toggle')
-  const theme = localStorage.getItem('theme')
+  html = document.querySelector("html");
+  btns = document.querySelectorAll("#theme-toggle");
+  const theme = localStorage.getItem("theme");
 
   // Check storage for theme
-  if (theme === 'light') setTheme('light', html)
-  else if (theme === 'dark') setTheme('dark', html)
+  if (theme === "light") setTheme("light", html);
+  else if (theme === "dark") setTheme("dark", html);
 
-  btn.addEventListener('click', toggleTheme)
+  btns.forEach((btn) => btn.addEventListener("click", toggleTheme));
 }
 
 function setTheme(theme, element) {
-  element.setAttribute('data-theme', theme)
-  localStorage.setItem('theme', theme)
+  element.setAttribute("data-theme", theme);
+  localStorage.setItem("theme", theme);
 }
 
 function toggleTheme() {
   // Check if theme was already set
-  if (html.getAttribute('data-theme') === 'light') setTheme('dark', html)
-  else if (html.getAttribute('data-theme') === 'dark') setTheme('light', html)
-  
+  if (html.getAttribute("data-theme") === "light") setTheme("dark", html);
+  else if (html.getAttribute("data-theme") === "dark") setTheme("light", html);
   // Fallback on user preference
-  else if (window.matchMedia('(prefers-color-scheme: light)').matches) setTheme('dark', html)
-  else if (window.matchMedia('(prefers-color-scheme: dark)').matches) setTheme('light', html)
+  else if (window.matchMedia("(prefers-color-scheme: light)").matches)
+    setTheme("dark", html);
+  else if (window.matchMedia("(prefers-color-scheme: dark)").matches)
+    setTheme("light", html);
 }
