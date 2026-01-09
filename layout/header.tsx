0a56bc6a8d28ed49cb22ca3e90770063540cedb0
Jacob Bleser <bleserjacob@gmail.com>
2026-01-09 18:58:06 -0500

    Remove unused code

--- layout/header.tsx
+++ layout/header.tsx
@@ -1,4 +1,4 @@
-import { Bluesky, Dots, Email, Github, LinkedIn, Sun } from "./icons.tsx"
+import { Bluesky, Sun } from "./icons.tsx"
 
 export default () => {
   return (
@@ -6,12 +6,6 @@ export default () => {
       <div class="profile">
         <a href="/"><img src="/img/card-gradient.jpg" alt="Profile picture" /></a>
         <h1><a href="/">Jacob Bleser</a></h1>
-        {/* <address>
-          <a href="mailto:bleserjacob@gmail.com"><Email /></a>
-          <a href="https://bsky.app/profile/jacobb.nyc"><Bluesky /></a>
-          <a href="https://github.com/megaboy101"><Github /></a>
-          <a href="https://linkedin.com/in/jacobbleser"><LinkedIn /></a>
-        </address> */}
       </div>
 
       <div class="settings" role="group">
